675eeb02f2c536f73b7ea34c50f1b35b95f26d1c
Mariano Troncoso <petotronco@gmail.com>
2020-04-12 16:41:33 -0300

    nueva hoja de estilos, primer componente, uso de fragment

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './componentes/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hola Mundo!</h1>
-    </div>
+    <Fragment>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </Fragment>
   );
 }
 
---
+++ src/componentes/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+// un componente siempre es una funcion
+function Header(){
+
+    // siempre tenemos que retornar algo
+    return(
+        // codigo 
+        <h1>Peto sos clave</h1>
+    )
+}
+
+// exportamos el componente
+export default Header;
